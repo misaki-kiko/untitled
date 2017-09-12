659e96b8f207304e05fd46f2c56d5305a479088e
misaki-kiko <misaki@futa.in>
2017-09-12 18:05:05 +0800

    练习4-21

--- main.c
+++ main.c
@@ -10,23 +10,19 @@ int main(void)
 {
     int i,j;
     int n;
-    printf(" |");
-    for (n=1;n<=9;n++)
-    {
-        printf("%3d",n);
-    }
-    printf("\n------------------------------");
-    printf("\n");
-    for (i=1;i<=9;i++)
-    {
-        printf("%d",i);
-        printf("|");
-        for (j=1;j<=9;j++){
-            printf("%3d",i*j);}
-        putchar('\n');
 
+    printf("正方形有几层？：");
+    scanf("%d",&n);
+
+    for (i=1;i<=n;i++)
+    {
+        putchar('*');
+        for (j=2;j<=n;j++)
+        {
+            putchar('*');
+        }
+        putchar('\n');
     }
     return 0;
 }
 
-//我感觉这一段肯定有什么迷一样的错误。。。。。
