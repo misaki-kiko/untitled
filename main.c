5b54f40907a0d4a9316627678bcd4cf2a607e691
misaki-kiko <misaki@futa.in>
2017-09-13 15:23:54 +0800

    练习4-24

--- main.c
+++ main.c
@@ -1,29 +1,29 @@
 /*
     练习用／2017 misaki_kiko
-    练习4-23第二部分 显示出直角在右上方的等腰三角形。
 */
 
 #include <stdio.h>
 
-int main() {
-    int width = 0;
+#include <stdio.h>
 
-    printf("输入一个正整数：");
-    scanf("%d",&width);
-
-    for (int i = 0; i < width; ++i) {
-        int num_of_space = i;
-        int num_of_star  = width - i;
-
-        for (int j = 0; j < num_of_space; ++j) {
+int main()
+{
+    int count;
+    scanf("%d",&count);
+    for (int i=0;i<count; ++i)
+    {
+        int num_of_space= count - i;
+        int num_of_star=i*2+1;
+        for (int j=0;j< num_of_space;++j)
+        {
             putchar(' ');
         }
 
-        for (int j = 0; j < num_of_star; ++j) {
+        for (int j=0; j<num_of_star;++j)
+        {
             putchar('*');
         }
 
         putchar('\n');
     }
-    return 0;
 }
