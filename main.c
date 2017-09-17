11328aed894af91a8afc9ab47ed4193c4a3c62f4
misaki-kiko <misaki@futa.in>
2017-09-17 18:00:46 +0800

    练习5-5

--- main.c
+++ main.c
@@ -3,19 +3,28 @@
 */
 
 #include <stdio.h>
+#define N 7
 
 int main(void) {
     int i;
-    int a[5]={17,23,36};
-    int b[5];
+    int x[N];
 
-    for (i=0;i<5;i++)
-        b[i]=a[4-i];
+    for (i=0;i<N;i++)
+    {
+        printf("x[%d]",i);
+        scanf("%d",&x[i]);
+    }
 
-    puts("  a    b");
-    puts("=========");
-    for (i=0;i<5;i++)
-        printf("%4d%4d\n",a[i],b[i]);
+    for(i=0;i<N/2;i++)
+    {
+        int temp=x[i];
+        x[i] = x[N-i-1];
+        x[N-i-1]=temp;
+    }
 
+    puts("倒序排列了。");
+    for (i = 0; i < N; ++i) {
+        printf("x[%d]=%d\n",i,x[i]);
+    }
     return 0;
 }
