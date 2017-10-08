3414de6778bd24d00b3766b9f8d01e6308042e9b
misaki-kiko <misaki@futa.in>
2017-10-08 18:51:01 +0800

    练习6-5

--- main.c
+++ main.c
@@ -3,30 +3,19 @@
 */
 
 #include <stdio.h>
-#define NUMBER 1000
+int cube (int x)
+{
+    int a;
+    a=x;
+    while (x-- >0)
+           a+=x;
+
+    return a;
+}
+
 int main(void) {
-    int num;
-    int m;
-    int x[m];
-
-    printf("数据个数：");
-    do
-    {
-        scanf("%d",&num);
-        if (num<0||num>NUMBER)
-        printf("请输入1～1000的数字：");
-    } while(num<0||num>NUMBER);
-
-    for (int i = 0; i <num ; ++i) {
-        printf("%2d号：",i+1);
-            scanf("%d",&x[i]);
-    }
-    printf("{");
-    for (int j = 0; j <num ; ++j) {
-        printf("%d",x[j]);
-        if (j != num - 1)
-        printf(",");
-    }
-    printf("}");
+    int a;
+     scanf("%d",&a);
+    printf("%d",cube(a));
     return 0;
 }
